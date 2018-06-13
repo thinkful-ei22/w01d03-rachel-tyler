3d36626178d94045e6f5d5489f7089e6da4864d0
rpinto <rachel.lea.pinto@gmail.com>
2018-06-13 09:18:36 -0700

    added second exercise

--- object-drills-1.js
+++ object-drills-1.js
@@ -10,3 +10,14 @@ function createMyObject(){
 },
 };
 }
+
+// Object updater
+
+function updateObject(obj) {
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
+
+    return obj;
+}
