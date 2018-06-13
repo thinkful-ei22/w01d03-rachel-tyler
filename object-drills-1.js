7e5db9b1184a925533e5d4937f296c788f465fc0
Tyler Warrick <tcw5038@gmail.com>
2018-06-13 09:10:59 -0700

    Added first exercise

--- object-drills-1.js
+++ object-drills-1.js
@@ -1,1 +1,12 @@
-'use strict'
+'use strict';
+//Object creator
+function createMyObject(){
+    return {
+    foo:'bar',
+    answerToUniverse: 42,
+    'olly olly':'oxen free',
+    sayHello: function(){
+        return 'hello';
+},
+};
+}
