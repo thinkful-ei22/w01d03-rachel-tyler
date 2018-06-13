d60698187cb3aec85fb6df5f9d29b2fc5fb17b7d
Tyler Warrick <tcw5038@gmail.com>
2018-06-13 09:24:34 -0700

    Added next exercise

--- object-drills-1.js
+++ object-drills-1.js
@@ -21,3 +21,17 @@ function updateObject(obj) {
 
     return obj;
 }
+
+//Self-reference
+function personMaker() {
+    var person = {
+      firstName: 'Paul',
+      lastName: 'Jones',
+      // replace `null` with a function that uses self reference to return
+      // full name
+      fullName: function() {
+          return (`${this.firstName} ${this.lastName}`);
+      },
+    };
+    return person;
+  }
